4e62e7d175e51123f50d7f285399dd9b08cbed0c
00AyanamiRei <kozlovmax27056@gmail.com>
2023-12-10 01:34:30 +0200

    First commit project

---
+++ kawaii-store/src/main/java/com/ayanami/kawaiistore/KawaiiStoreApplication.java
@@ -0,0 +1,13 @@
+package com.ayanami.kawaiistore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KawaiiStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KawaiiStoreApplication.class, args);
+	}
+
+}
---
+++ kawaii-store/src/test/java/com/ayanami/kawaiistore/KawaiiStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ayanami.kawaiistore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KawaiiStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
